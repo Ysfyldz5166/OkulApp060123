06f9cc0debc1985124874d91d23f7853f9d6e1e6
Yusuf yıldız <103460234+Ysfyldz5166@users.noreply.github.com>
2024-01-06 14:53:29 +0300

    Add files via upload

---
+++ OkulApp.DAL/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OkulApp.DAL
+{
+    public class Helper
+    {
+        SqlConnection _connection=null;
+        SqlCommand _command=null;
+            string cstr = ConfigurationManager.ConnectionStrings["cstr"].ConnectionString;//appconfig içerisindeki connectionString için gerekli kodları çektim.
+        public int ExecuteNonQuery(string cmdtext, SqlParameter[] parameters=null)
+        {
+            using (_connection = new SqlConnection(cstr))
+            {
+                using (_command=new SqlCommand(cmdtext,_connection))
+                {
+                    if (parameters != null)
+                    {
+                        _command.Parameters.AddRange(parameters);
+
+                    }
+                    _connection.Open();
+                    return _command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ OkulApp.MODEL/OgretmenModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OkulApp.MODEL
+{
+    public class OgretmenModel
+    {
+        public string TcNo { get; set; }
+        public string Name { get; set;}
+        public string SurName { get; set; }
+        public string TelNo { get; set; }
+    }
+}
